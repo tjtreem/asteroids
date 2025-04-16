55b56c89f33e919345486d5372b22026d3f6f2d2
tjtreem <tjtreem@gmail.com>
2025-04-16 14:12:08 -0400

    Implement basic game loop with pygame window

---
+++ main.py
@@ -0,0 +1,29 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    
+    while True:  # This creates an infinite loop
+        # Check for quit events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  # Exit the function, ending the game
+        
+        # Fill the screen with black
+        screen.fill("black")  # Note: we use screen.fill, not pygame.fill
+        
+        # Update the display
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
